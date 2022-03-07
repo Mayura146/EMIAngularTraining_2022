0ab0c1f23d1328030f71e46744834f08e1047cf7
vamshi <vamshichowdary130@gmail.com>
2022-03-07 17:33:53 +0530

    AddingRoutingForEmployee

--- FeatureModule/src/app/FeatureModules/Employee/EmployeeModule/employees-routing.module.ts
+++ FeatureModule/src/app/FeatureModules/Employee/EmployeeModule/employees-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { DeleteEmployeeComponent } from './delete-employee/delete-employee.component';
+import { EditEmployeeComponent } from './edit-employee/edit-employee.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"employeeList",component:EmployeeListComponent,children:[
+    {path:"add",component:AddEmployeeComponent},
+    {path:"edit",component:EditEmployeeComponent},
+    {path:"delete",component:DeleteEmployeeComponent}
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- FeatureModule/src/app/app.module.ts
+++ FeatureModule/src/app/app.module.ts
@@ -1,24 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AddEmployeeComponent } from './FeatureModules/Employee/EmployeeModule/add-employee/add-employee.component';
-import { EditEmployeeComponent } from './FeatureModules/Employee/EmployeeModule/edit-employee/edit-employee.component';
-import { DeleteEmployeeComponent } from './FeatureModules/Employee/EmployeeModule/delete-employee/delete-employee.component';
-import { EmployeeListComponent } from './FeatureModules/Employee/EmployeeModule/employee-list/employee-list.component';
+import { EmployeesModule } from './FeatureModules/Employee/EmployeeModule/employees.module';
+// import { AddEmployeeComponent } from './FeatureModules/Employee/EmployeeModule/add-employee/add-employee.component';
+// import { EditEmployeeComponent } from './FeatureModules/Employee/EmployeeModule/edit-employee/edit-employee.component';
+// import { DeleteEmployeeComponent } from './FeatureModules/Employee/EmployeeModule/delete-employee/delete-employee.component';
+// import { EmployeeListComponent } from './FeatureModules/Employee/EmployeeModule/employee-list/employee-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddEmployeeComponent,
-    EditEmployeeComponent,
-    DeleteEmployeeComponent,
-    EmployeeListComponent
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    EmployeesModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
