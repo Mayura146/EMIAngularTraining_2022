578a975cb5bfd9552040103be065e4adef759506
mayura.sonar <mayura.sonar@outlook.com>
2022-03-14 10:44:20 +0530

    AngularForms

--- AngularForms/src/app/app.module.ts
+++ AngularForms/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TemplateFormComponent } from './Components/template-form/template-form.component';
+import { ReactiveFormComponent } from './Components/reactive-form/reactive-form.component';
+import { FormArrayComponent } from './Components/form-array/form-array.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateFormComponent,
+    ReactiveFormComponent,
+    FormArrayComponent
   ],
   imports: [
     BrowserModule,
