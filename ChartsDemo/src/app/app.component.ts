573480e9b2f13170a7c06f6681b18ad5d2bba989
mayura.sonar <mayura.sonar@outlook.com>
2022-03-21 18:16:19 +0530

    chartdemo

--- ChartsDemo/src/app/app.component.ts
+++ ChartsDemo/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ChartData, ChartOptions } from 'chart.js';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ChartsDemo';
+  MobileSales: ChartData<'line'>={
+    labels:['Samsung','OnePlus','IPhone','Vivo'],
+    datasets:[
+      {label:'Jan',data:[1000,4000,780,890]},
+      {label:'Feb',data:[2000,8000,780,345]},
+      {label:'March',data:[1000,4000,780,9087]},
+    ]
+    
+  };
+  chartOptions:ChartOptions={
+    responsive: true,
+    plugins:{
+    title:{
+    
+      text:'Mobile Sales Data'
+    },
+  }
+  }
 }
