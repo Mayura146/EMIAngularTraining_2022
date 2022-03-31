c1a86aa6051c4fa610fda2ea74544beb96cc0900
mayura.sonar <mayura.sonar@outlook.com>
2022-03-31 10:04:28 +0530

    sds

--- RouteGuardRole/src/app/app.module.ts
+++ RouteGuardRole/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './Components/about/about.component';
+import { HomeComponent } from './Components/home/home.component';
+import { AddEmployeeComponent } from './Components/add-employee/add-employee.component';
+import { EditEmployeeComponent } from './Components/edit-employee/edit-employee.component';
+import { EmployeeComponent } from './Components/employee/employee.component';
+import { LoginComponent } from './Components/login/login.component';
+import { PageNotFoundComponent } from './Components/page-not-found/page-not-found.component';
+import { ProfileComponent } from './Components/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    HomeComponent,
+    AddEmployeeComponent,
+    EditEmployeeComponent,
+    EmployeeComponent,
+    LoginComponent,
+    PageNotFoundComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
